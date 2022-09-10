0bafb047cf92bec60dd98c8fe492ecf1e0bfd842
TseoH <assikevin10@gmail.com>
2022-09-10 02:39:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ *main - printf
+ *
+ *Description: it's print gramar way
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
