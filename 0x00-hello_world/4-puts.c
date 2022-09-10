a712816ffae02088e1ae656d4732036ab630d9db
TseoH <assikevin10@gmail.com>
2022-09-10 02:25:20 -0700

    Update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
+ *main - prinft
  *
  *
- * Main string with puts
+ * Description: funcion that show 'Progamming is fun'
+ * Return: 0
+ * 
  */
-
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
