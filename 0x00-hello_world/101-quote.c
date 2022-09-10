b5dcb1d00c777014faab23dc42a39341dc12d080
TseoH <assikevin10@gmail.com>
2022-09-10 05:21:21 -0700

    Update 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,12 +9,24 @@
  */
 int main(void)
 {
-	char *s = (char *) "and that piece of art is useful\" - "
-		"Dora Korpar, 2015-10-19\n";
+	char *s = (char *) "and that piece of art is useful\" - ";
 
-	int length = strlen(s);
+	char *t =(char *) "Dora Korpar, 2015-10-19\n";
 
-	write(1, s, length);
+	unsigned int length1 = strlen(s);
+
+	unsigned int length2 =strlen(t);
+
+	unsigned int sum = length1+length2;
+
+	char *str;
+
+	str = malloc(length1+length2);
+
+	strcpy(str, s);
+	strcat(str, t);
+
+	write(1, str, sum);
 
 	return (1);
 }
