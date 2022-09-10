745f048eda7a457263d80f022be86c1b1d5dbd80
TseoH <assikevin10@gmail.com>
2022-09-10 04:54:37 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ *main - Entry Point
+ *
+ *Description: write foo
+ *Return: 1
+ */
+int main(void)
+{
+	char * str = (char *) "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int length = strlen(str);
+	write(1, str, length);
+	return (1);
+}
