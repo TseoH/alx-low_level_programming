6351872a22aa9f605bc48ac2e4fe0488047a3982
TseoH <assikevin10@gmail.com>
2022-09-10 16:42:12 -0700

    Udapte 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,6 @@
 /* more headers goes there */
 
 #include <stdio.h>
-#include <string.h>
 
 /* betty style doc for function main goes there */
 /**
@@ -21,42 +20,22 @@ int main(void)
   n = rand() - RAND_MAX / 2;
   /* your code goes there */
 
-  char *defautl_str = (char *)"Last digit of %d and is ";
-
-  char *five_str = (char *)"greater than 5";
-
-  char *zero_str = (char *)"0";
-
-  char *six_str = (char *)"less than 6 and not 0";
-   
   if(n < 6 && n != 0)
   {
-    char *mgs = malloc(strlen(defautl_str) + strlen(six_str));
-    
-    strcpy(mgs, defautl_str);
-
-    strcat(mgs, six_str);
-
-    printf(mgs, n);
+	  printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
+  
   }
   else if (n > 5)
   {
-    char *mgs =  malloc(strlen(defautl_str) + strlen(five_str));
-    strcpy(mgs, defautl_str);
 
-    strcat(mgs, five_str);
-
-    printf(mgs, n);
-
+    printf("Last digit of %d is %d and is greater than 5", n, n % 10);
+  
   }
   else
   {
-    char *mgs =  malloc(strlen(defautl_str) + strlen(zero_str));
-    strcpy(mgs, defautl_str);
 
-    strcat(mgs, zero_str);
-
-    printf(mgs, n);
+    printf("Last digit of %d is %d and is 0", n, n % 10);
+  
   }
   
   return (0);
