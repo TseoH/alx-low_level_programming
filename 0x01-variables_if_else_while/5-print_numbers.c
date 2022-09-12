6407dadcaeb3effa78b8d75a5599f5b274cbf5c4
TseoH <assikevin10@gmail.com>
2022-09-12 16:04:09 -0700

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ *
+ * Description: number
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
