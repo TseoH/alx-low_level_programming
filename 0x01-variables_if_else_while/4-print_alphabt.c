7b90c3941870d082f8277b7b1f293c9097336a98
TseoH <assikevin10@gmail.com>
2022-09-12 15:49:52 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - entry
+ *
+ * Description: Alphbet
+ * Return: 0
+ */
+int main(void)
+{
+	for (i = 'a'; i < 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	return (0);
+}
