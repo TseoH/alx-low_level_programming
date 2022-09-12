097742d83edd432607af9f127eca7750c9b10174
TseoH <assikevin10@gmail.com>
2022-09-12 12:22:13 -0700

    Update 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,17 +5,18 @@
 * Main is entry
  *
  * Description: Print alphabet with put-char
+ * Return: 0
 */
 int main(void)
 {
-    char letters[] = "abcdefghijklmnopqrstuvwxyz\n";
-    
-    long unsigned int i = 0;
-    
-    for (i = 0; i < strlen(letters); i++)
-    {
-        putchar(letters[i]);
-    }
-  
-    return (0);
+	char letters[] = "abcdefghijklmnopqrstuvwxyz\n";
+	
+	long unsigned int i = 0;
+	
+	for(i = 0; i < strlen(letters); i++)
+	{
+		putchar(letters[i]);
+	}
+	
+	return (0);
 }
