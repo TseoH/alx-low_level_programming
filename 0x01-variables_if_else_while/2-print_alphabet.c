61135cd2c9a5389d05d8bf94ec66d5f566e4c179
TseoH <assikevin10@gmail.com>
2022-09-12 15:15:06 -0700

    Update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,12 @@
 */
 int main(void)
 {
-	char letters[] = "abcdefghijklmnopqrstuvwxyz\n";	
+	char letters[] = "abcdefghijklmnopqrstuvwxyz\n";
 	size_t i = 0;
 
 	for (i = 0; i < strlen(letters); i++)
 	{
 		putchar(letters[i]);
 	}
-	
 	return (0);
 }
