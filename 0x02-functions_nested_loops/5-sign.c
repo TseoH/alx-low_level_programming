c4742f1cff6be6c87adc93bab086a4174ee7bc0c
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-13 21:53:31 +0000

    Update 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_sign - print print_sign
+*
+* Description : print_sign printer
+* @n: param
+* Return: 1 if True
+* Or 0 if False
+*/
+int print_sign(int n)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
