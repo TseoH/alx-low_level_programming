7f8fcd18aeb42ede517a5cd200f50fc2f06989ac
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-13 21:24:47 +0000

    Update 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* islower - print islower
+*
+* Description : islower
+* Return: 1 if True
+* Return: 0 if False
+*/
+int _islower(int c)
+{
+    return (islower(c));
+}
