d190c546a85c15970c1968a735fc3b338328b4f6
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-14 23:01:16 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,2 +1,38 @@
 #include "main.h"
 
+/**
+* times_table - print times_table
+*
+* Description : times_table printer
+* Return: void
+*/
+void times_table(void)
+{
+        int x, y, i;
+        
+        for (x = 0; x <= 9; x++)
+        {
+                _putchar('0');
+                _putchar(',');
+                _putchar(' ');
+                for (y = 1; y <= 9; y++)
+                {
+                        d = (x * y);
+                        if ((i / 10) > 0)
+                        {
+                                _putchar((i / 10) + '0');
+                        }
+                        else
+                        {
+                                _putchar(' ');
+                        }
+                        _putchar((i % 10) + '0');
+                        if (y < 9)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                }
+                _putchar('\n');
+        }
+}
