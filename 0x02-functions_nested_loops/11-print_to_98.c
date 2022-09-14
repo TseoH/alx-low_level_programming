bc812c3195f3687f8e596f7ccb8b149d097b6efc
TseoH <assikevin10@gmail.com>
2022-09-14 16:58:04 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98 - foo
+ *
+ * Description: print_to_98
+ * @n: param
+ * Return: 0
+ */
+int print_to_98(int n)
+{
+	if (n < 98)
+	{
+		int i = 0;
+
+		while (i <= 98)
+		{
+			_putchar((n + i), '0');
+			i++;
+
+		}
+		_putchar('\n');
+	}
+	else if (n > 98)
+	{
+		int i = n;
+
+		while (i >= 98)
+		{
+			_putchar(i + '0');
+		}
+	}
+}
