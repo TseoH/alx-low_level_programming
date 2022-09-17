0e8332e8f3bd7070178862695bb85bcb8030b3c4
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-17 08:57:08 +0000

    Update some ways

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,11 @@
 */
 int main(int argc, char const *argv[])
 {
-        for (int i = 0; i < 10; i++)
+        int i;
+        int y;
+        for (i = 0; i < 10; i++)
         {
-                for (int y = 0; y < 10; y++)
+                for (y = 0; y < 10; y++)
                 {
                         if (i < y)
                         {
