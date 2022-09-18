a48231adde483c67079351eec84697879373d8cf
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-18 10:36:02 +0000

    Update some ways

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,26 +6,28 @@
  * Description: main
  * Return: 0
  */
-int main(void) {
-        int i = 0;
-        unsigned long prev = 0;
-        unsigned long current = 1;
-        unsigned long result = 0;
+int main(void)
+{
+	int i = 0;
+	unsigned long prev = 0;
+	unsigned long current = 1;
+	unsigned long result = 0;
 
-        while (i < 50) {
-            result = prev + current;
-            prev = current;
-            current = result;
-            i++;
-            printf("%lu", result);
-            if(i == 49)
-            {
-                    printf("\n");
-            }
-            else
-            {
-                    printf(", ");
-            }
-        }
-        return (0);
+	while (i < 50)
+	{
+	    result = prev + current;
+	    prev = current;
+	    current = result;
+	    printf("%lu", result);
+	    if(i == 49)
+	    {
+		    printf("\n");
+	    }
+	    else
+	    {
+		    printf(", ");
+	    }
+	    i++;
+	}
+	return (0);
 }
