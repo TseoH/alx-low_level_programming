d9a6524122fb8ca35f7184a21d79d046fe192f2c
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-18 08:34:59 +0000

    Update some ways

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
-* print_last_digit - print print_last_digit
+* print_x - print print_x
 *
-* Description : print_last_digit printer
+* Description : print_x printer
 * @n: param
 * Return: 1 if True
 * Or 0 if False
 */
-int print_last_digit(int n)
+int print_x(int n)
 {	int x = n;
 
 	x = (n % 10);
+	if (x < 0)
+	x = x * -1;
 	return (x);
 }
