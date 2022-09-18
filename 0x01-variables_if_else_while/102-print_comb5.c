db23aaf7f85f2a6c41a4b18cb95e026adadc71f7
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-18 07:51:28 +0000

    Update some ways

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,27 @@
 */
 int main(void)
 {
-        int i;
-        int y;
-        for (i = 0; i <= 98; i++)
-        {
-                for (y = 0; y <= 99; y++)
-                {
-                        putchar((i / 10) + '0');
-                        putchar((i % 10) + '0');
-                        putchar(' ');
-                        putchar((y / 10) + '0');
-                        putchar((y % 10) + '0');
-                        if (i == 98 && y == 99)
-                        {
-                            break;
-                        }
-                        else
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-        return 0;
+	int i, y;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (y = i + 1; y <= 99; y++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (i == 98 && y == 99)
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
