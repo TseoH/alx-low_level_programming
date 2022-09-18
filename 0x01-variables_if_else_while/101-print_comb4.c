74a41aff5d10bb62d3f11c761e61b7edd4857b87
TseoH <assikevin10@gmail.com>
2022-09-18 01:18:38 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - entry
 *
@@ -8,33 +7,34 @@
 */
 int main(void)
 {
-        int i;
-        int y;
-        int z;
-
-        for (i = 0; i < 10; i++)
-        {
-                for (y = 0; y < 10; y++)
-                {
-                        for (z = 0; z < 10; z++) {
-                                if (i < y && y < z)
-                                {
-                                        putchar(i + '0');
-                                        putchar(y + '0');
-                                        putchar(z + '0');
-                                        if (i == 7 && y == 8 && z == 9)
-                                        {
-                                                continue;
-                                        }
-                                        else
-                                        {
-                                                putchar(',');
-                                                putchar(' ');
-                                        }
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return 0;
+	int i;
+	int y;
+	int z;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			for (z = 0; z < 10; z++)
+			{
+				if (i < y && y < z)
+				{
+					putchar(i + '0');
+					putchar(y + '0');
+					putchar(z + '0');
+					if (i == 7 && y == 8 && z == 9)
+					{
+						continue;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
 }
