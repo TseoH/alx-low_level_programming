54d48adc765340a11dca91b6e1354ab463127e50
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-18 10:43:33 +0000

    Add Fibona

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - foo
+ *
+ * Description: main
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long prev = 0;
+	unsigned long current = 1;
+	unsigned long result = 0;
+        unsigned long event_result = 0;
+
+	while (i < 50)
+	{
+		result = prev + current;
+		prev = current;
+		current = result;
+                if(result % 2 == 0)
+                {
+                        event_result += result;
+                }
+		i++;
+	}
+        printf("%lu\n", event_result);
+	return (0);
+}
