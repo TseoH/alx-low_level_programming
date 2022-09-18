a111d33b5e20e6bda862fb86b982b68c5f1a9536
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-18 08:23:35 +0000

    Update some ways

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,28 +8,28 @@
 */
 int main(void)
 {
-        int i;
-        int y;
-        for (i = 0; i < 10; i++)
-        {
-                for (y = 0; y < 10; y++)
-                {
-                        if (i < y)
-                        {
-                                putchar(i + '0');
-                                putchar(y + '0');
-                                if (y == 9 && i == 8)
-                                {
-                                        continue;
-                                }
-                                else
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return 0;
+	int i;
+	int y;
+	for (i = 0; i < 10; i++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (i < y)
+			{
+				putchar(i + '0');
+				putchar(y + '0');
+				if (y == 9 && i == 8)
+				{
+					continue;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
 }
