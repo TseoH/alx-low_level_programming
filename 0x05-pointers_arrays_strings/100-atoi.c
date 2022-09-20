b70bf6fb34ff8ac4af1caab7415085e186993dcd
Kevin'S Assi <44835795+TseoH@users.noreply.github.com>
2022-09-20 21:12:55 +0000

    Update 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*_atoi - printf _atoi
+*
+*Description: _atoi
+*@s: String
+*Return: void
+*/
+int _atoi(char *s)
+{
+	return atoi(s);
+}
