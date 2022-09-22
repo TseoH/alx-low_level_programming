d8b8897a62c93d63975ad01a7c47ad162085ca1b
TseoH <assikevin10@gmail.com>
2022-09-22 21:26:51 +0000

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,13 +3,13 @@
 #include <string.h>
 
 /**
-* _strncpy - print _strncpy
+*_strncpy - print _strncpy
 *
-* Description: print _strncpy
-* @dest : String
-* @src : String
-* @n : int
-* Return : char *
+*Description: print _strncpy
+*@dest : String
+*@src : String
+*@n : int
+*Return: A char String
 */
 char *_strncpy(char *dest, char *src, int n)
 {
