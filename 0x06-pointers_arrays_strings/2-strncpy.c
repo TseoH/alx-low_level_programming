4c49796b1e7bebf383ef444a6ff520f98b120baf
TseoH <assikevin10@gmail.com>
2022-09-22 21:25:34 +0000

    Update 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _strncpy - print _strncpy
+*
+* Description: print _strncpy
+* @dest : String
+* @src : String
+* @n : int
+* Return : char *
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *new_dest;
+	char *result;
+	int i = 0;
+
+	new_dest = malloc(strlen(dest));
+	while (dest[i] != '\0')
+	{
+		new_dest[i] = dest[i];
+		i++;
+	}
+	return (strncpy(new_dest, src, n));
+}
