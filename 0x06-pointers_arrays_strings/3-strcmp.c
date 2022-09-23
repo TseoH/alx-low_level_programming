849667fe3aecf29e70ec00f0017e64b733a43e38
TseoH <assikevin10@gmail.com>
2022-09-23 07:12:53 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,11 +15,13 @@ int _strcmp(char *s1, char *s2)
 	int s1_sum = 0;
 	int s2_sum = 0;
 
-	for (i = 0; i < strlen(s1); ++i) {
+	for (i = 0; i < strlen(s1); ++i)
+	{
 		s1_sum += s1[i];
 	}
-	for (i = 0; i < strlen(s2); ++i) {
+	for (i = 0; i < strlen(s2); ++i)
+	{
 		s2_sum += s2[i];
 	}
-	return s1_sum - s2_sum;
+	return (s1_sum - s2_sum);
 }
