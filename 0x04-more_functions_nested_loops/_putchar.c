e10cc903c745be56d809cc99b359a127252fd7ed
TseoH <assikevin10@gmail.com>
2022-09-26 07:11:29 +0000

    Add _putchar for testing

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_putchar - printf _putchar
+*
+*Description: _putchar print
+*@c: Integer
+*Return: Void
+*/
+void _putchar(int c)
+{
+	putchar(c);
+}
