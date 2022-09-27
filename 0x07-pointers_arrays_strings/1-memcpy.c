655fba2d7fbe5d5ace0264458d8bd44690c8bf07
TseoH <assikevin10@gmail.com>
2022-09-27 21:39:05 +0000

    Add 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_memcpy - printf _memcpy
+*
+*Description: _memcpy print
+*@dest: Char
+*@src: Char
+*@n: Unsigned Integer
+*Return: Void
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; ++i) {
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
