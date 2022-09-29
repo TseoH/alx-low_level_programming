51eb9848c316f8bb3fd14ce3ff129e7ef915686c
TseoH <assikevin10@gmail.com>
2022-09-29 21:50:53 +0000

    Update recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,14 +11,11 @@
 int _strlen_recursion(char *s)
 {
 	int i = 0;
-	if (*s)
-	{
-		i++;
-		_strlen_recursion(s + 1);
-		return (i + 1);
-	}
-	else
+
+	if (*s == 0)
 	{
 		return (0);
 	}
+	i = _strlen_recursion(s + 1);
+	return (i + 1);
 }
