e67992b1aa60fe661cb95097edd77d84652a6e98
TseoH <assikevin10@gmail.com>
2022-09-30 09:16:58 +0000

    Update 0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int b;
 	int r;
 
-	if(argc > 2)
+	if (argc > 2)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - printf foo name
+*
+*Description: main print
+*@argc: Integer
+*@argv: String array
+*/
+int main(int argc, char *argv[])
+{
+	int contain_letter = 0;
+	int i;
+	int r = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; ++i)
+		{
+			if (*argv[i] != 48 && atoi(argv[i]) == 0)
+			{
+				contain_letter = 1;
+				break;
+			}
+			else
+			{
+				r += atoi(argv[i]);
+			}
+		}
+		if (contain_letter)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			printf("%d\n", r);
+			return (0);
+		}
+	}
+	printf("0\n");
+	return (0);
+}
