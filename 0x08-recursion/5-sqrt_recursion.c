1ff1f71288d5013d7fc4822a666d6aefd4a178e1
TseoH <assikevin10@gmail.com>
2022-09-30 08:16:16 +0000

    Update 0x0A-argc_argv

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_pow_recursion - printf _pow_recursion
+*
+*Description: _pow_recursion print
+*@n: Integer
+*Return: Integer
+*/
+int _sqrt_recursion(int n)
+{
+	int i = n;
+	int x = i / 2;
+	if ((x * x) == n)
+	{
+		return x;
+	}
+}
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - printf foo name
+*
+*Description: main print
+*@argc: Integer
+*@argv: String array
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d", *argv[0]);
+	return (0);
+}
