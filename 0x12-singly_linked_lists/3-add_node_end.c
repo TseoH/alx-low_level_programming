ecfdf0dc078d2eae62519f45f7b88fae8b8539af
TseoH <assikevin10@gmail.com>
2022-10-22 18:02:31 +0000

    Update due to Betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = len;
 	new_node->next = NULL;
 
-	if (*head == NULL){
+	if (*head == NULL)
+	{
 		*head = new_node;
 	}
 	else
