65b9847eb30a1a1764f8352e626a4e4aff87bdb6
TseoH <assikevin10@gmail.com>
2022-11-15 13:09:00 +0000

    Update no mandatory files

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,3 +1,46 @@
-//
-// Created by Kevin de Djamo on 15/11/2022.
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the opcodes of its own main function
+ * @argc: arguments number
+ * @argv: arguments
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
