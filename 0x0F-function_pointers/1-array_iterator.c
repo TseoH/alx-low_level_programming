aa385d6bc4661a7756854e883bccf75a5e682ffc
TseoH <assikevin10@gmail.com>
2022-11-15 12:50:38 +0000

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,21 @@
-//
-// Created by Kevin de Djamo on 15/11/2022.
-//
+#include "function_pointers.h"
+
+/**
+ * array_iterator - array_iterator
+ * @array: array
+ * @size: array size.
+ * @action: pointer function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
