df9ce6511522c984a02c3c09a01f4bd4074f41f1
TseoH <assikevin10@gmail.com>
2022-11-15 12:55:20 +0000

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,27 @@
-//
-// Created by Kevin de Djamo on 15/11/2022.
-//
+#include "function_pointers.h"
+
+/**
+ * int_index - find integer in int list
+ * @array: int list
+ * @size: list size
+ * @cmp: pointer function
+ *
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
